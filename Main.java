f85ad36383218df567551c7e784ad7540054f574
Waewarin Chindarassami <w.chindarassami@gmail.com>
2022-06-22 17:35:32 -0500

    Add Main class

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
+import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
     public static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        try
+        {
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
 }
