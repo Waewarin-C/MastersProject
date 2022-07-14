48db4ae68d2a4d82e024b0959801ad0fd81e7ac9
Waewarin Chindarassami <w.chindarassami@gmail.com>
2022-07-14 14:12:51 -0500

    Layout changes

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -8,9 +11,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage)
     {
+        stage = primaryStage;
+
         try
         {
-
+            Parent root = FXMLLoader.load(getClass().getResource("./view/Welcome.fxml"));
+            stage.setScene(new Scene(root));
+            stage.show();
         }
         catch(Exception e)
         {
