09a49f352d136b245ba8c952ce5068d665d06e29
Waewarin Chindarassami <w.chindarassami@gmail.com>
2022-07-14 13:26:45 -0500

    Add Account class for login and create account

---
+++ Model/Account.java
@@ -0,0 +1,11 @@
+package Model;
+
+//Takes care of
+    //Login
+        //Checks that the credentials are correct
+    //Create account
+        //Checks all fields are entered
+        //Checks all fields are correct
+
+public class Account {
+}
