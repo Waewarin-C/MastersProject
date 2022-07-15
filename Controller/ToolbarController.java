13ec3adbadd43b6c353c18a66270066e5fe8c6a6
Waewarin Chindarassami <w.chindarassami@gmail.com>
2022-07-15 14:42:03 -0500

    Add toolbar view and controller

---
+++ Controller/ToolbarController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+//Controller for the Toolbar view
+//Toolbar view will include
+    //Home
+    //Events Calendar
+    //Notes Calendar
+    //Categories
+    //Settings
+    //This will be a bottom navigation bar on each of the main pages
+
+public class ToolbarController {
+}
