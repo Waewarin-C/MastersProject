908c7521a47668e793ae23de2410b6079aa38b55
Waewarin Chindarassami <w.chindarassami@gmail.com>
2022-08-14 16:13:27 -0500

    Working on Home page

--- Application/Controller/HomeController.java
+++ Application/Controller/HomeController.java
@@ -10,14 +10,18 @@ package Application.Controller;
 
 import Application.Main;
 import Application.Model.Event;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -29,6 +33,9 @@ import java.util.ResourceBundle;
 public class HomeController implements Initializable {
     @FXML
     private Label helloMessage, todayDateLabel;
+
+    @FXML
+    private GridPane upcomingEvents;
 
     @FXML
     private VBox todayEvents, sevenDayEvents;
@@ -69,70 +76,92 @@ public class HomeController implements Initializable {
 
     private void displayEvents()
     {
-        displayTodayEvents();
-        displayNextSevenDayEvents();
-    }
-
-    private void displayTodayEvents()
-    {
-        Label firstEvent = new Label();
-        firstEvent.setStyle("-fx-font: 18px \"Berlin Sans FB\";");
-
-        if(!Main.login.getUser().getEvents().containsKey(date))
-        {
-            firstEvent.setText("You have no events today");
-        }
-        else
-        {
-            List<Event> events = Main.login.getUser().getEvents().get(date);
-            String eventName = events.get(0).getEventName();
-            firstEvent.setText(eventName);
-
-            int numMoreEvents = events.size() - 1;
-            if(numMoreEvents > 0)
-            {
-                String moreEventText = "+ " + numMoreEvents + " more";
-                Label moreEvents = new Label(moreEventText);
-                moreEvents.setStyle("-fx-font: 16px \"Berlin Sans FB\";");
-                todayEvents.getChildren().add(moreEvents);
-            }
-        }
-
-        todayEvents.getChildren().add(0, firstEvent);
-    }
-
-    private void displayNextSevenDayEvents()
-    {
         int week = 7;
 
-        for(int i = 1; i <= week; i++)
+        for(int i = 0; i < week; i++)
         {
             LocalDate nextDate = today.plusDays(i);
             String next = nextDate.format(format);
 
-            Label eventLabel = new Label();
-            eventLabel.setStyle("-fx-font: 18px \"Berlin Sans FB\";");
+            VBox dayEvents = new VBox();
+            dayEvents.setSpacing(5);
+            dayEvents.setPadding(new Insets(0, 10, 0, 10));
 
+            Label dayLabel = new Label();
+            dayLabel.setStyle("-fx-font: 20px \"Berlin Sans FB\";");
+            if(i == 0)
+            {
+                dayLabel.setText("Today: " + next);
+            }
+            else
+            {
+                dayLabel.setText(next);
+            }
+            dayEvents.getChildren().add(dayLabel);
+
+            Label eventLabel = new Label();
+            eventLabel.setStyle("-fx-font: 16px \"Berlin Sans FB\";");
             if(!Main.login.getUser().getEvents().containsKey(next))
             {
-                String noEvents = next + ": You have no events";
-                eventLabel.setText(noEvents);
-                sevenDayEvents.getChildren().add(eventLabel);
+                eventLabel.setText("You have no events");
+                dayEvents.getChildren().add(eventLabel);
             }
             else
             {
                 List<Event> events = Main.login.getUser().getEvents().get(next);
-                String eventName = next + ": " + events.get(0).getEventName();
+                String eventName = events.get(0).getEventName();
                 eventLabel.setText(eventName);
-                sevenDayEvents.getChildren().add(eventLabel);
+                dayEvents.getChildren().add(eventLabel);
 
                 int numMoreEvents = events.size() - 1;
                 if(numMoreEvents > 0)
                 {
                     String moreEventText = "+ " + numMoreEvents + " more";
                     eventLabel.setText(eventName + "\t.\t.\t.\t" + moreEventText);
+                    addViewAllEventsButton(i+1, next);
+                }
+                else
+                {
+                    addViewEventDetailsButton(i+1, next);
                 }
             }
+            upcomingEvents.add(dayEvents, 0, i+1);
         }
+    }
+
+    private void addViewAllEventsButton(int row, String date)
+    {
+        Button viewEvents = new Button();
+        viewEvents.setPrefSize(Region.USE_COMPUTED_SIZE, 30);
+        viewEvents.setStyle("-fx-font: 14px \"Berlin Sans FB\";");
+        viewEvents.setText("View All Events for " + date);
+
+        EventHandler<ActionEvent> viewAllEvents = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        };
+        viewEvents.setOnAction(viewAllEvents);
+
+        upcomingEvents.add(viewEvents, 1, row);
+    }
+
+    private void addViewEventDetailsButton(int row, String date)
+    {
+        Button viewEventDetails = new Button();
+        viewEventDetails.setPrefSize(Region.USE_COMPUTED_SIZE, 30);
+        viewEventDetails.setStyle("-fx-font: 14px \"Berlin Sans FB\";");
+        viewEventDetails.setText("View Event Details for " + date);
+
+        EventHandler<ActionEvent> viewDetails = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        };
+        viewEventDetails.setOnAction(viewDetails);
+
+        upcomingEvents.add(viewEventDetails, 1, row);
     }
 }
